58cd779c680f4b7cbe01306e9e8f76f104b19870
Peter <peter.prentiss@gmail.com>
2017-11-11 20:06:17 -0600

    Input sets term state.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -8,7 +8,9 @@ class SearchBar extends Component {
     }
     render() {
         return (
-            <input onChange={event => console.log(event.target.value)} />
+            <div>
+                <input onChange={event => this.setState({ term: event.target.value })} />
+            </div>
         )
     }
 }
