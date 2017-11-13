1a62e1d2c316589b63f37a96fd785181ab89474e
Peter <peter.prentiss@gmail.com>
2017-11-12 21:05:09 -0600

    Implement YouTube search.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+
 import SearchBar from './components/SearchBar';
 
 const API_KEY = 'AIzaSyBI71IWoxEsq7WFNuEHLnp-yucGJx9tSXA';
+
+YTSearch({ key: API_KEY, term: 'surfboards '}, function(data) {
+    console.log(data);
+});
+
 const App = () => {
     return (
         <div>
