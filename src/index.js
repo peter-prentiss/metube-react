ce28c4d6e6b924613ee3e174ecb101fd686768ea
Peter <peter.prentiss@gmail.com>
2017-11-14 17:37:03 -0600

    Refactor App to separate class component

--- src/index.js
+++ src/index.js
@@ -1,22 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
-
-import SearchBar from './components/SearchBar';
-
+import App from './components/App';
 const API_KEY = 'AIzaSyBI71IWoxEsq7WFNuEHLnp-yucGJx9tSXA';
 
 YTSearch({ key: API_KEY, term: 'surfboards '}, function(data) {
     console.log(data);
 });
 
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    );
-};
+// const App = () => {
+//     return (
+//         <div>
+//             <SearchBar />
+//         </div>
+//     );
+// };
 
 ReactDOM.render(
     <App />,
