ba210d3e0d3f6d91774645a9f9bdb19c2519b8af
Peter <peter.prentiss@gmail.com>
2017-11-15 14:02:28 -0600

    Input search displays live results in list.

--- src/components/app.js
+++ src/components/app.js
@@ -14,17 +14,22 @@ class App extends Component {
             selectedVideo: null
         };
 
-        YTSearch({ key: API_KEY, term: 'surfboards ' }, videos => {
-            this.setState({ 
+        this.videoSearch('surfboards');
+    }
+
+    videoSearch(term) {
+        YTSearch({ key: API_KEY, term: term }, videos => {
+            this.setState({
                 videos: videos,
                 selectedVideo: videos[0]
-             })
+            });
         });
     }
+
     render() {
         return (
             <div>
-                <SearchBar />
+                <SearchBar onSearchTermChange={term => this.videoSearch(term)}/>
                 <VideoDetail video={this.state.selectedVideo} />
                 <VideoList 
                     onVideoSelect={selectedVideo => this.setState({selectedVideo})}
