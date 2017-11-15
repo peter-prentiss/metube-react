d9616ab6978d4a1fc7771c73d1c461634ca2d495
Peter <peter.prentiss@gmail.com>
2017-11-15 02:37:39 -0600

    Set up video list display

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
 import SearchBar from './SearchBar';
+import VideoList from './VideoList'
+import YTSearch from 'youtube-api-search';
+const API_KEY = 'AIzaSyBI71IWoxEsq7WFNuEHLnp-yucGJx9tSXA';
 
 class App extends Component {
+    constructor(props) {
+        super(props)
 
+        this.state = { videos: [] };
+
+        YTSearch({ key: API_KEY, term: 'surfboards ' }, videos => {
+            this.setState({ videos })
+        });
+    }
     render() {
         return (
             <div>
                 <SearchBar />
+                <VideoList videos={this.state.videos} />
             </div>
         );
     }
--- src/index.js
+++ src/index.js
@@ -1,20 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import YTSearch from 'youtube-api-search';
 import App from './components/App';
-const API_KEY = 'AIzaSyBI71IWoxEsq7WFNuEHLnp-yucGJx9tSXA';
-
-YTSearch({ key: API_KEY, term: 'surfboards '}, function(data) {
-    console.log(data);
-});
-
-// const App = () => {
-//     return (
-//         <div>
-//             <SearchBar />
-//         </div>
-//     );
-// };
 
 ReactDOM.render(
     <App />,
