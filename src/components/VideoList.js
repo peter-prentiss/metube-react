4c268c5f7a9758174cfa4e2c3589f319416735c5
Peter <peter.prentiss@gmail.com>
2017-11-15 13:53:44 -0600

    Clicking on video list items updates selected video

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -3,8 +3,13 @@ import VideoListItem from './VideoListItem';
 
 const VideoList = props => {
     const videoItems = props.videos.map(video => {
-        return <VideoListItem video={video} key={video.etag} />
-    })
+        return (
+            <VideoListItem 
+                onVideoSelect={props.onVideoSelect}
+                video={video} 
+                key={video.etag} />
+        );
+    });
 
     return (
         <ul className="col-md-4 list-group">
